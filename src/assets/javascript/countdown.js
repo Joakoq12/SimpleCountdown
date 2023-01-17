7e8bf0279729c1738061a21e110b70effb58ca8e
Joakoq12 <fullestnewyork@gmail.com>
2023-01-17 11:56:59 -0500

    Actualizacion con webpack

--- src/assets/javascript/countdown.js
+++ src/assets/javascript/countdown.js
@@ -34,3 +34,13 @@ var myDate = document.querySelector('#myDate');
 if(myDate.getAttribute('value') != '2023-10-15T12:00'){
     myDate.style.bottom = '100%';
 }
+
+function background1() {
+  document.querySelector('body').style.background = "url('./assets/img/su-san-lee-E_eWwM29wfU-unsplash.jpg') center center / cover";
+}
+function background2() {
+  document.querySelector('body').style.background = "url('./assets/img/pexels-mikhail-nilov-8430372.jpg') center center / cover";
+}
+function background3() {
+  document.querySelector('body').style.background = "url('./assets/img/pexels-pixabay-460672.jpg') center center / cover";
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+
+
+
+import './assets/javascript/background';
+import './assets/javascript/countdown';
+import './assets/css/index.css';
